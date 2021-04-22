97eadc5a0c42d6fabfa7413465c28b6c74d680d0
Flávio Silva <63196828+flaviojnsilva@users.noreply.github.com>
2021-04-22 20:39:42 +0100

    Initial commit

---
+++ src/RunLengthEncoding.java
@@ -0,0 +1,120 @@
+import java.util.concurrent.TimeUnit;
+
+public class RunLengthEncoding {
+
+    public static void main(String[] args) {
+        encode("teeeeessste");
+        decode("e1r1t1r10");
+    }
+
+
+    private static String encode(String input) {
+
+        long nano1 = System.nanoTime();
+
+        StringBuilder result = new StringBuilder();
+        int lengthOfInput = input.length();
+        char lastCharacter = input.charAt(0);
+        int lastCharacterCount = 1;
+
+        // we will go until equal to the length of input and we will do the final flush inside the loop
+        // in this way we will have roughly 90% - 95% performance improvent over nested iteration
+        // but this might take a little more mory as compared to the nested loop iteration
+        for (int index = 1; index <= lengthOfInput; index++) {
+
+            if (index == lengthOfInput) {
+                // we have already completed everything let us append the final value of index - 1 iteration
+                result.append(lastCharacter).append(lastCharacterCount);
+                break;
+            }
+
+            char currentCharacter = input.charAt(index);
+
+            if (lastCharacter == currentCharacter) {
+
+                lastCharacterCount++;
+            } else {
+
+                result.append(lastCharacter).append(lastCharacterCount);
+                lastCharacter = currentCharacter;
+                lastCharacterCount = 1;
+            }
+        }
+
+        long nano2 = System.nanoTime();
+
+        long result1 = TimeUnit.NANOSECONDS.toMicros(nano2 - nano1);
+
+        System.out.println("encoding total time taken : nano seconds -> " + result1);
+        System.out.println(result);
+        return result.toString();
+    }
+
+
+    private static String decode(String encoded) {
+
+        long nano1 = System.nanoTime();
+
+        StringBuilder result = new StringBuilder();
+        int lengthOfEncodedString = encoded.length();
+
+        StringBuilder timesToRepeatLastCharacter = new StringBuilder("");
+        char lastCharacter = encoded.charAt(0);
+
+        for (int index = 1; index <= lengthOfEncodedString; index++) {
+
+            if (index == lengthOfEncodedString) {
+                // we have reached to the end of encoding ; do the final round
+                // this code looks repeated
+                if (timesToRepeatLastCharacter.length() != 0) {
+                    for (int i = 0; i < Integer.parseInt(timesToRepeatLastCharacter.toString()); i++) {
+                        result.append(lastCharacter);
+                    }
+                    break;
+                } else {
+                    System.out.println("Erro de Leitura");
+                    return null;
+                }
+
+            }
+
+            char currentCharacter = encoded.charAt(index);
+
+            if (Character.isDigit(currentCharacter)) {
+
+                timesToRepeatLastCharacter.append(currentCharacter);
+            } else {
+
+                if (timesToRepeatLastCharacter.length() != 0) {
+
+                    // try parsing the timesToRepeatLastCharacter and get the number of times the character should be repeated
+                    for (int i = 0; i < Integer.parseInt(timesToRepeatLastCharacter.toString()); i++) {
+                        result.append(lastCharacter);
+                    }
+
+                    lastCharacter = currentCharacter;
+                    timesToRepeatLastCharacter = new StringBuilder();
+                } else {
+                    System.out.println("Erro de Leitura");
+                    return null;
+                }
+
+
+
+            }
+
+        }
+
+        long nano2 = System.nanoTime();
+
+        long result1 = TimeUnit.NANOSECONDS.toMicros(nano2 - nano1);
+
+        System.out.println("decoding total time taken : nano seconds -> " + result1);
+        System.out.println(result);
+
+        return result.toString();
+    }
+
+
+}
+
